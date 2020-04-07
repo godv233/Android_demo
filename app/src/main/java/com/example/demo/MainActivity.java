7029f1650d94d9dbad30e55ed4c67150d6ccb27d
godv233 <zengwei233@126.com>
2020-04-07 23:59:35 +0800

    TextView

--- app/src/main/java/com/example/demo/MainActivity.java
+++ app/src/main/java/com/example/demo/MainActivity.java
@@ -3,12 +3,26 @@ package com.example.demo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.edit_text);
+        //按钮绑定事件,点一下设置不可用
+        btn= (Button)findViewById(R.id.btn1);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (btn.getText().toString().equals("按钮")){
+                    btn.setEnabled(false);
+                    btn.setText("按钮不可用");
+                }
+            }
+        });
     }
 }
