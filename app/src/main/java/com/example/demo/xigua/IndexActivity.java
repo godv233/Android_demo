ee9bbe06528242b1ff05c3020a04e9f402fa0c20
godv233 <zengwei233@126.com>
2020-04-10 15:54:18 +0800

    模仿西瓜视频首页

---
+++ app/src/main/java/com/example/demo/xigua/IndexActivity.java
@@ -0,0 +1,16 @@
+package com.example.demo.xigua;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.demo.R;
+
+public class IndexActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.index_layout);
+    }
+}
