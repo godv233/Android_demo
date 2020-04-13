9e51d2194e7894a0ae131c4bfb675425780f8a58
godv233 <zengwei233@126.com>
2020-04-13 23:05:17 +0800

    RecycleView(2)

--- app/src/main/java/com/example/demo/RecycleActivity.java
+++ app/src/main/java/com/example/demo/RecycleActivity.java
@@ -4,10 +4,14 @@ import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
 import com.example.demo.adapter.RecycleAdapter;
+
+import static androidx.recyclerview.widget.StaggeredGridLayoutManager.*;
 
 /**
  * @Author godv
@@ -21,12 +25,22 @@ public class RecycleActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.recycle_layout);
         recyclerView = findViewById(R.id.recycleView);
-        //垂直方向和水平方向的滚动列表
-        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
-        recyclerView.setLayoutManager(linearLayoutManager);
-        //设置间隔
-        recyclerView.addItemDecoration(new SpaceItemDecoration(50));
         RecycleAdapter recycleAdapter = new RecycleAdapter(this);
+        //垂直方向和水平方向的滚动列表
+//        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+//        recyclerView.setLayoutManager(linearLayoutManager);
+//        //设置间隔
+//        recyclerView.addItemDecoration(new SpaceItemDecoration(50));
+//        recyclerView.setAdapter(recycleAdapter);
+        //网格布局
+//        GridLayoutManager gridLayoutManager=new GridLayoutManager(this,3);
+//        recyclerView.setLayoutManager(gridLayoutManager);
+//        recyclerView.addItemDecoration(new SpaceItemDecoration(20));
+//        recyclerView.setAdapter(recycleAdapter);
+        //瀑布流 需要设置加载不同的图片才能偶效果 固定图片  就和网格一样了
+        StaggeredGridLayoutManager staggeredGridLayoutManager=new StaggeredGridLayoutManager(2, VERTICAL);
+        recyclerView.setLayoutManager(staggeredGridLayoutManager);
         recyclerView.setAdapter(recycleAdapter);
+        recyclerView.addItemDecoration(new SpaceItemDecoration(20));
     }
 }
