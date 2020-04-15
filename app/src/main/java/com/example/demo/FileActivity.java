3ace8459b6ba0c2a1ee2eed317f00a7399dc638f
godv233 <zengwei233@126.com>
2020-04-15 11:31:13 +0800

    File

---
+++ app/src/main/java/com/example/demo/FileActivity.java
@@ -0,0 +1,76 @@
+package com.example.demo;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.IOException;
+
+/**
+ * @Author godv
+ * Date on 2020/4/14  22:02
+ */
+public class FileActivity extends AppCompatActivity {
+    private EditText editname;
+    private EditText editdetail;
+    private Button btnsave;
+    private Button btnclean;
+    private Button btnread;
+    private Context mContext;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.file_layout);
+        mContext = getApplication();
+        editdetail = (EditText) findViewById(R.id.editdetail);
+        editname = (EditText) findViewById(R.id.editname);
+        btnclean = (Button) findViewById(R.id.btnclean);
+        btnsave = (Button) findViewById(R.id.btnsave);
+        btnread = (Button) findViewById(R.id.btnread);
+        //清空
+        btnclean.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editdetail.setText("");
+                editname.setText("");
+            }
+        });
+        //保存
+        btnsave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FileHelper fHelper = new FileHelper(mContext);
+                String filename = editname.getText().toString();
+                String filedetail = editdetail.getText().toString();
+                try {
+                    fHelper.save(filename, filedetail);
+                    Toast.makeText(getApplicationContext(), "数据写入成功", Toast.LENGTH_SHORT).show();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    Toast.makeText(getApplicationContext(), "数据写入失败", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        btnread.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String detail = "";
+                FileHelper fHelper2 = new FileHelper(getApplicationContext());
+                try {
+                    String fname = editname.getText().toString();
+                    detail = fHelper2.read(fname);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                Toast.makeText(getApplicationContext(), detail, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
